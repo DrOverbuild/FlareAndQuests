3b9bae0f744af1916a2d910a2fa061a86ed0f60a
Jasper Reddin <jazzbeenjamin@me.com>
2016-08-10 20:46:20 -0500

    Separate onInvClick() into separate methods
    I do this so I can work on onInvClick() while being able to collapse
    most of the code.

--- src/main/java/com/drizzard/faq/listeners/InventoryListener.java
+++ src/main/java/com/drizzard/faq/listeners/InventoryListener.java
@@ -98,102 +98,117 @@ public class InventoryListener implements Listener {
 	}
 
 	@EventHandler
-	public void onInvClick(final InventoryClickEvent ev) {
+	public void onInvClick(InventoryClickEvent ev) {
 		if (ev.getInventory().getTitle().contains("Set Flare Items For ")) {
-			ItemStack cursor = ev.getCursor();
-			ItemStack clicked = ev.getCurrentItem();
-			boolean clBAD = clicked == null || clicked.getType() == Material.AIR;
-			boolean cuBAD = cursor == null || cursor.getType() == Material.AIR;
-
-			int slot = ev.getRawSlot();
-
-			final ItemStack target;
-			if (ev.getClick() == ClickType.SHIFT_LEFT && slot >= 27 && !clBAD)
-				target = clicked;
-			else if (slot < 27 && !cuBAD)
-				target = cursor;
-			else
-				target = null;
-
-			if (target != null) {
-				final ItemStack targetClone = target.clone();
-				String t = ev.getInventory().getTitle();
-				final String name = t.substring(t.indexOf("For ") + 4);
-
-				new BukkitRunnable() {
-					public void run() {
-						ItemMeta im = targetClone.getItemMeta();
-						List<String> lore = im.getLore();
-						double chance = 100.0;
-						if (lore != null) {
-							String str = ChatColor.stripColor(lore.get(lore.size() - 1));
-							try {
-								chance = Double.parseDouble(str);
-								lore.remove(lore.size() - 1);
-								im.setLore(lore);
-								targetClone.setItemMeta(im);
-							} catch (Exception e) {
-								chance = 100;
-							}
-						}
-
-						ItemStack tag = new ItemStack(Material.NAME_TAG);
-						im = tag.getItemMeta();
-						im.setDisplayName(chance + "");
-						tag.setItemMeta(im);
-
-						AnvilSender.send(ev.getWhoClicked(), "Set Chance", tag);
-
-						plugin.getAnvils().put(ev.getWhoClicked().getOpenInventory().getTopInventory(), new Group<ItemStack, String>(targetClone, name));
-					}
-				}.runTaskLater(plugin, 1);
-			}
+			flareItemsModified(ev);
 		} else if (ev.getInventory() instanceof AnvilInventory) {
-			ItemStack clicked = ev.getCurrentItem();
-
-			if (clicked == null || ev.getRawSlot() > 3) {
-				return;
-			}
-
-			if (clicked.getType().equals(Material.AIR)) {
-				return;
-			}
-
-			Group<ItemStack, String> g = plugin.getAnvils().remove(ev.getInventory());
-			if (g == null)
-				return;
-
-			ev.setCancelled(true);
-
-			ItemStack target = g.a;
-			String name = g.b;
-
-			ItemMeta im = target.getItemMeta();
-			List<String> lore = im.getLore();
-			if (lore == null) {
-				lore = new ArrayList<String>();
-			}
-
-			if (clicked.hasItemMeta() && clicked.getItemMeta().hasDisplayName()) {
-				lore.add(clicked.getItemMeta().getDisplayName());
-			} else {
-				lore.add("100");
-			}
-
-			im.setLore(lore);
-			target.setItemMeta(im);
-
-			plugin.getConf().load();
-			List<ItemStack> items = (List<ItemStack>) plugin.getConf().config.getList("Flares." + name + ".Contents", new ArrayList<ItemStack>());
-			items.add(target);
-			plugin.getConf().config.set("Flares." + name + ".Contents", items);
-			plugin.getConf().save();
-
-			ev.getInventory().setContents(new ItemStack[2]);
-
-			plugin.openFlareInventory((Player) ev.getWhoClicked(), name);
+			itemChanceSet(ev);
 		}
 	}
 
+	/**
+	 * Called by onInvClick(InventoryClickEvent) when the inventory name
+	 * contains "Set Flare Items For", meaning the clicked inventory is for the
+	 * flare configuration stage.
+	 */
+	private void flareItemsModified(final InventoryClickEvent ev) {
+		ItemStack cursor = ev.getCursor();
+		ItemStack clicked = ev.getCurrentItem();
+		boolean clBAD = clicked == null || clicked.getType() == Material.AIR;
+		boolean cuBAD = cursor == null || cursor.getType() == Material.AIR;
 
+		int slot = ev.getRawSlot();
+
+		final ItemStack target;
+		if (ev.getClick() == ClickType.SHIFT_LEFT && slot >= 27 && !clBAD)
+			target = clicked;
+		else if (slot < 27 && !cuBAD)
+			target = cursor;
+		else
+			target = null;
+
+		if (target != null) {
+			final ItemStack targetClone = target.clone();
+			String t = ev.getInventory().getTitle();
+			final String name = t.substring(t.indexOf("For ") + 4);
+
+			new BukkitRunnable() {
+				public void run() {
+					ItemMeta im = targetClone.getItemMeta();
+					List<String> lore = im.getLore();
+					double chance = 100.0;
+					if (lore != null) {
+						String str = ChatColor.stripColor(lore.get(lore.size() - 1));
+						try {
+							chance = Double.parseDouble(str);
+							lore.remove(lore.size() - 1);
+							im.setLore(lore);
+							targetClone.setItemMeta(im);
+						} catch (Exception e) {
+							chance = 100;
+						}
+					}
+
+					ItemStack tag = new ItemStack(Material.NAME_TAG);
+					im = tag.getItemMeta();
+					im.setDisplayName(chance + "");
+					tag.setItemMeta(im);
+
+					AnvilSender.send(ev.getWhoClicked(), "Set Chance", tag);
+
+					plugin.getAnvils().put(ev.getWhoClicked().getOpenInventory().getTopInventory(), new Group<ItemStack, String>(targetClone, name));
+				}
+			}.runTaskLater(plugin, 1);
+		}
+	}
+
+	/**
+	 * Called by onInvClick(InventoryClickEvent) when the inventory is an anvil.
+	 */
+	private void itemChanceSet(InventoryClickEvent ev) {
+		ItemStack clicked = ev.getCurrentItem();
+
+		if (clicked == null || ev.getRawSlot() > 3) {
+			return;
+		}
+
+		if (clicked.getType().equals(Material.AIR)) {
+			return;
+		}
+
+		Group<ItemStack, String> g = plugin.getAnvils().remove(ev.getInventory());
+		if (g == null) {
+			return;
+		}
+
+		ev.setCancelled(true);
+
+		ItemStack target = g.a;
+		String name = g.b;
+
+		ItemMeta im = target.getItemMeta();
+		List<String> lore = im.getLore();
+		if (lore == null) {
+			lore = new ArrayList<String>();
+		}
+
+		if (clicked.hasItemMeta() && clicked.getItemMeta().hasDisplayName()) {
+			lore.add(clicked.getItemMeta().getDisplayName());
+		} else {
+			lore.add("100");
+		}
+
+		im.setLore(lore);
+		target.setItemMeta(im);
+
+		plugin.getConf().load();
+		List<ItemStack> items = (List<ItemStack>) plugin.getConf().config.getList("Flares." + name + ".Contents", new ArrayList<ItemStack>());
+		items.add(target);
+		plugin.getConf().config.set("Flares." + name + ".Contents", items);
+		plugin.getConf().save();
+
+		ev.getInventory().setContents(new ItemStack[2]);
+
+		plugin.openFlareInventory((Player) ev.getWhoClicked(), name);
+	}
 }
